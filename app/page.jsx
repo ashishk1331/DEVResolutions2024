b1eef3c6b1d73589b42051e289b2a57aead8c27c
Ashish Khare <akstyles211@gmail.com>
2024-01-26 01:37:12 +0530

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Card from "@/components/Card.jsx";
+import Printout from "@/components/Printout.jsx";
+
+import { useRef, useState } from "react";
+
+export default function Home() {
+  const card = useRef();
+  const [goal1, setGoal1] = useState("Publish an article in a new tag");
+  const [goal2, setGoal2] = useState("Set a goal for comments on DEV posts");
+  const [goal3, setGoal3] = useState("Share my weekly wins");
+
+  return (
+    <main className="flex flex-col md:flex-row min-h-screen items-center justify-between p-8">
+      <div className="w-full flex">
+        <Card ref={card} goals={[goal1, goal2, goal3]} />
+      </div>
+      <Printout
+        card={card}
+        goals={[goal1, goal2, goal3]}
+        setGoals={[setGoal1, setGoal2, setGoal3]}
+      />
+    </main>
+  );
+}
---
+++ components/Card.jsx
@@ -0,0 +1,61 @@
+import { forwardRef } from 'react';
+
+export default forwardRef(Card);
+
+function Card(props, ref) {
+	const { goals } = props;
+
+	return (
+		<div className="w-[600px] h-[600px] bg-[#4241FF] p-8 m-auto" ref={ref}>
+			<div className="flex flex-col gap-3 items-left text-black p-4 h-full bg-[#7270FF] rounded-xl border-2 border-black font-medium">
+				<div className="flex items-top gap-4">
+					<div className="balls flex items-center gap-2">
+						<div className="w-3 h-3 bg-black rounded-full border-2 border-black" />
+						<div className="w-3 h-3 border-2 border-black rounded-full" />
+						<div className="w-3 h-3 border-2 border-black rounded-full" />
+					</div>
+					<div className="w-fit bg-white rounded-t-xl p-2 px-4 border-2 border-black w-fit flex items-center gap-8">
+						#DEVResolutions2024
+						<X />
+					</div>
+				</div>
+				<div className="w-full bg-white rounded-xl p-1 px-4 border-2 border-black">
+					<p>dev.to</p>
+				</div>
+				<div className="flex flex-col items-left justify-around h-full w-full bg-black text-white rounded-xl p-2 px-6">
+					<h1 className="text-4xl font-bold mx-auto">
+						My (Dev)eloper Goals
+					</h1>
+					<div className="w-full h-[2px] rounded-full bg-white -my-4" />
+					{goals.map((title) => (
+						<Goal text={title} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function Goal({ text }) {
+	return (
+		<div className="flex items-center gap-8 px-2">
+			<div className="w-10 h-10 aspect-square rounded-md border-2 border-white" />
+			<p className="text-3xl font-light">{text}</p>
+		</div>
+	);
+}
+
+function X() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			fill="#000000"
+			viewBox="0 0 256 256"
+			className="w-4 h-4 stroke-4 stroke-black"
+		>
+			<path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>
+		</svg>
+	);
+}
